8db359c0e94f4befcf495c924e215f3c36f1ca37
Jodice Family <jodicefamily@Jodices-MBP.fios-router.home>
2023-12-15 20:24:18 -0500

    initial commit

--- code/post-analysis.R
+++ code/post-analysis.R
@@ -1,1 +1,19 @@
-#-
+#- library
+options(warn=-1)
+library(tidyverse)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(tidyr)
+library(gganimate)
+library(magick)
+library(REdaS)
+library(zoo)
+library(caret)
+library(keras)
+library(tensorflow)
+library(pROC)
+
+`%ni%`<- Negate(`%in%`)
+
+
